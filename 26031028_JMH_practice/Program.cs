22ad00abbd3d06bd822439b0d7a995bd1b1886df
정민후 <hu070605@gmail.com>
2026-08-30 19:45:44 +0900

    자기이력서

--- 26031028_JMH_practice/Program.cs
+++ 26031028_JMH_practice/Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("              --------------------------------              ");
+            Console.WriteLine("                                                            ");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("                         자기 이력서                        ");
+            Console.WriteLine("                                                            ");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("              --------------------------------              ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("                                                            ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("                        NAME  │ 정민후                     ");
+            Console.WriteLine("                                                            ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("                        ID    │ 26031028                   ");
+            Console.WriteLine("                                                            ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("                        AGE   │ 20살                       ");
+            Console.WriteLine("                                                            ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                        MBTI  │ INFP                       ");
+            Console.WriteLine("                                                            ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("                        취미  │ 게임                       ");
+            Console.WriteLine("                                                            ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("                  좋아하는 것 │ 맛있는 음식                ");
+            Console.WriteLine("                                                            ");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("                  싫어하는 것 │ 해산물                     ");
+            Console.WriteLine("                                                            ");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("                       키     │ 178 cm                     ");
+            Console.WriteLine("                                                            ");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("                       몸무게 │ 100 kg                     ");
+            Console.WriteLine("                                                            ");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("                       시력   │ 0.5 / 1.0                  ");
+            Console.WriteLine("                                                            ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("                       혈액형 │ -O형                       ");
+            Console.WriteLine("                                                            ");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
